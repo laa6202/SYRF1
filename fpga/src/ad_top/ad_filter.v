9d469cccfc930989b51338cc1cb28672227cf285
安 卢 <laa6203@163.com>
2017-09-07 14:36:45 +0800

    add docs

---
+++ fpga/src/ad_top/ad_filter.v
@@ -0,0 +1,25 @@
+//ad_filter.v
+
+module ad_filter(
+ad_data_s1,
+ad_vld_s1,
+ad_data_s2,
+ad_vld_s2,
+//clk rst
+clk_sys,
+rst_n
+);
+input [15:0]	ad_data_s1;
+input					ad_vld_s1;
+output [15:0]	ad_data_s2;
+output				ad_vld_s2;
+//clk rst
+input	clk_sys;
+input	rst_n;
+//--------------------------------------
+//--------------------------------------
+
+
+
+endmodule
+
